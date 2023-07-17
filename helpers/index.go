75bc6454ce12eca935211e78a063ccdd566348ac
Chimezie <princecee15@gmail.com>
2023-07-18 00:01:23 +0100

    added json helpers

--- helpers/index.go
+++ helpers/index.go
@@ -1,12 +1,20 @@
 package helpers
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
 
 type Helpers struct{}
+
+type ResponseData struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+}
 
 func (h *Helpers) Logger(f http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -15,4 +23,25 @@ func (h *Helpers) Logger(f http.Handler) http.HandlerFunc {
 		f.ServeHTTP(w, r)
 		fmt.Printf("%d ms\n", time.Since(now).Milliseconds())
 	}
+}
+
+func (h *Helpers) writeJSON(w http.ResponseWriter, data ResponseData, code int) error {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	w.WriteHeader(code)
+	w.Write(jsonData)
+
+	return nil
+}
+
+func (h *Helpers) readJSON(r *http.Request, data *any) error {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(body, data)
 }
